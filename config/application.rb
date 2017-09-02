f566de44850ea41653f53b20f618e6bcdb1d2820
Susumu Yamazaki <zacky1972@gmail.com>
2017-09-02 14:00:01 +0000

    change TZ to Asia/Tokyo

--- config/application.rb
+++ config/application.rb
@@ -14,5 +14,8 @@ module Swest
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    
+    config.time_zone = 'Asia/Tokyo'
+    config.active_record.default_timezone = :local
   end
 end
